2dd9604511f00955a863586c261b28b006e7d33a
Jacob Evans <cloud887@gmail.com>
2021-01-17 17:00:30 -0600

    Pretter, TS & ESlint not completely working together yet

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,9 @@
-const autoPreprocess = require('svelte-preprocess');
+const autoPreprocess = require("svelte-preprocess");
 
 module.exports = {
   preprocess: autoPreprocess({
     defaults: {
-      script: 'typescript',
+      script: "typescript",
     },
   }),
 };
