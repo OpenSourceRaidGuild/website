180da14ad1a836d72355da52a6cfbf212b6f3de4
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2021-01-19 08:40:32 +1100

    fix: formatting in svelte files

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import { Router, Route } from 'svelte-navigator'
+  import { Router, Route } from 'svelte-navigator'
 
-	import QuestBoard from './routes/quests/index.svelte'
-	import Raids from './routes/dungeons/raids/index.svelte'
-	import ViewRaid from './routes/dungeons/raids/ViewRaid.svelte'
-	import NotFound from './routes/NotFound.svelte'
+  import QuestBoard from './routes/quests/index.svelte'
+  import Raids from './routes/dungeons/raids/index.svelte'
+  import ViewRaid from './routes/dungeons/raids/ViewRaid.svelte'
+  import NotFound from './routes/NotFound.svelte'
 </script>
 
 <Router>
-	<Route path="/">
-		<h1>Welcome to the Open Source Raid Guild</h1>
-	</Route>
-	<Route path="quests" component={QuestBoard} />
-	<Route path="raids" component={Raids} />
-	<Route path="raids/:id" component={ViewRaid} />
-	<Route path="*" component={NotFound} />
+  <Route path="/">
+    <h1>Welcome to the Open Source Raid Guild</h1>
+  </Route>
+  <Route path="quests" component={QuestBoard} />
+  <Route path="raids" component={Raids} />
+  <Route path="raids/:id" component={ViewRaid} />
+  <Route path="*" component={NotFound} />
 </Router>
--- frontend/src/components/UserStatBlock.svelte
+++ frontend/src/components/UserStatBlock.svelte
@@ -1,70 +1,70 @@
 <script lang="ts">
-	import { blur } from 'svelte/transition'
-	import Card from './Card.svelte'
+  import { blur } from 'svelte/transition'
+  import Card from './Card.svelte'
 
-	export let rank: number
-	export let userStats: UserStats
+  export let rank: number
+  export let userStats: UserStats
 </script>
 
 <Card let:className>
-	<li class={`${className} user-stat-block`} in:blur>
-		<p class="rank">#{rank}</p>
-		<img
-			class="avatar"
-			src={userStats.avatarUrl}
-			alt="{userStats.user}'s Profile Image"
-		/>
-		<p class="name">{userStats.user}</p>
-		<div class="stat-block">
-			<p>
-				<span class="emoji-stat-light">⚔️️️️️️</span
-				>+{userStats.additions}&emsp;-{userStats.deletions}
-			</p>
-			<p>
-				<span class="emoji-stat-light">💾</span>{userStats.commits}
-				{userStats.commits === 1 ? 'Commit' : 'Commits'}
-			</p>
-		</div>
-	</li>
+  <li class={`${className} user-stat-block`} in:blur>
+    <p class="rank">#{rank}</p>
+    <img
+      class="avatar"
+      src={userStats.avatarUrl}
+      alt="{userStats.user}'s Profile Image"
+    />
+    <p class="name">{userStats.user}</p>
+    <div class="stat-block">
+      <p>
+        <span class="emoji-stat-light">⚔️️️️️️</span
+        >+{userStats.additions}&emsp;-{userStats.deletions}
+      </p>
+      <p>
+        <span class="emoji-stat-light">💾</span>{userStats.commits}
+        {userStats.commits === 1 ? 'Commit' : 'Commits'}
+      </p>
+    </div>
+  </li>
 </Card>
 
 <style>
-	.user-stat-block {
-		margin-bottom: 1rem;
-		display: grid;
-		grid-gap: 2rem;
-		grid-template-columns: 3.75rem 6rem 8.365rem 3fr;
-		grid-template-areas: 'rank avatar name stats';
-	}
+  .user-stat-block {
+    margin-bottom: 1rem;
+    display: grid;
+    grid-gap: 2rem;
+    grid-template-columns: 3.75rem 6rem 8.365rem 3fr;
+    grid-template-areas: 'rank avatar name stats';
+  }
 
-	.rank {
-		font-size: 2rem;
-		opacity: 0.8;
-		grid-area: rank;
-		align-self: center;
-	}
+  .rank {
+    font-size: 2rem;
+    opacity: 0.8;
+    grid-area: rank;
+    align-self: center;
+  }
 
-	.avatar {
-		border-radius: 50%;
-		width: 6rem;
-		height: 6rem;
-		grid-area: avatar;
-		align-self: center;
-	}
+  .avatar {
+    border-radius: 50%;
+    width: 6rem;
+    height: 6rem;
+    grid-area: avatar;
+    align-self: center;
+  }
 
-	.name {
-		grid-area: name;
-		align-self: center;
-	}
+  .name {
+    grid-area: name;
+    align-self: center;
+  }
 
-	.stat-block {
-		grid-area: stats;
-		align-self: center;
-	}
+  .stat-block {
+    grid-area: stats;
+    align-self: center;
+  }
 
-	.emoji-stat-light {
-		font-size: 1.25rem;
-		margin-right: 1rem;
-		opacity: 0.8;
-	}
+  .emoji-stat-light {
+    font-size: 1.25rem;
+    margin-right: 1rem;
+    opacity: 0.8;
+  }
 </style>
