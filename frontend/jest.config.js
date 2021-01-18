2fde112a994e9d65cd5b13c373701a679869152a
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2021-01-18 17:33:52 +1100

    fix: eslint module undef

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  transform: {
-    '^.+\\.svelte$': [
-      'svelte-jester',
-      {
-        preprocess: true,
-      }
-    ],
-    '^.+\\.ts$': 'ts-jest',
-  },
-  moduleFileExtensions: ['js', 'ts', 'svelte'],
-  moduleNameMapper: {
-    '^#components/(.*)': '<rootDir>/src/components/$1',
-  },
-  setupFilesAfterEnv: ['./src/setupTests.ts'],
+	transform: {
+		'^.+\\.svelte$': [
+			'svelte-jester',
+			{
+				preprocess: true,
+			},
+		],
+		'^.+\\.ts$': 'ts-jest',
+	},
+	moduleFileExtensions: ['js', 'ts', 'svelte'],
+	moduleNameMapper: {
+		'^#components/(.*)': '<rootDir>/src/components/$1',
+	},
+	setupFilesAfterEnv: ['./src/setupTests.ts'],
 }
