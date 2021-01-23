8dae12ae0e9b67cbfb6b25428c17bb73e90e9703
Abraham Prince <abraham@Abrahams-MacBook-Pro.local>
2021-01-23 18:20:58 -0500

    convert card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: 'white',
+        border: '1px solid hsl(0, 0%, 78%)',
+        borderRadius: '8px',
+        padding: '1rem 2rem',
+      }}
+    >
+      Card
+    </div>
+  )
+}
