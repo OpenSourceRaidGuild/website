ec53d30c64af55e962562a7cd87dd4c0c922c2e9
abeprincec <abeprincec@gmail.com>
2021-01-23 20:23:45 -0500

    Convert Card Component (#30)
    * convert card component
    
    * convert card component
    
    * convert card component
    
    Co-authored-by: Abraham Prince <abraham@Abrahams-MacBook-Pro.local>

---
+++ src/components/card/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+interface Props {
+  children?: React.ReactNode
+}
+
+function Card({ children }: Props) {
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: 'white',
+        border: '1px solid hsl(0, 0%, 78%)',
+        borderRadius: '8px',
+        padding: '1rem 2rem',
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Card
