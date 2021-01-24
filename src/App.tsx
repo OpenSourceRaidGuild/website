c13145000d7d0f257a070dca399164b6b2a1cf12
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2021-01-24 13:39:31 +0300

    Global Styles #33

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+/**@jsx jsx */
+import { Global, jsx } from '@emotion/react'
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import LoadingSpinner from './components/loading-spinner'
 import routes from './route.config'
+import { globalStyles } from './styles/globalStyles'
 
 const App = () => (
   <Router>
@@ -11,6 +14,7 @@ const App = () => (
         {routes.map((route, i) => (
           <Route key={i} {...route} />
         ))}
+        <Global styles={globalStyles} />
       </Switch>
     </React.Suspense>
   </Router>
