0fc31274264953756be1ec1d8a112f4907d81c26
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2021-01-26 00:16:19 +0300

    added precommit test comand

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // '*.+(js|jsx|ts|tsx)': ['eslint', 'yarn test'],
+  // '*.+(js|jsx|ts|tsx)': ['eslint', 'yarn test:precommit'],
   '*.+(js|jsx|ts|tsx)': ['yarn lint'],
   '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
     'prettier --write',
