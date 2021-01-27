8cf2ecff4efb401cf8daf898982e586e3ebdf550
ahmedeldessouki <nemoahmed@hotmail.co.uk>
2021-01-27 13:13:46 +0300

    requested changes

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   '*.+(js|jsx|ts|tsx)': ['yarn lint:precomit'],
-  // '*.+(js|jsx|ts|tsx)': ['yarn lP:precommit', 'yarn test:precommit'],
   '*.+(json|yml|yaml|css)': ['prettier --write'],
 }
