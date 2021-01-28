bc0d76dc0ac327e3130ffc55f8b654aa22eace6e
codyarose <crose992@gmail.com>
2021-01-28 02:14:22 -0600

    refactor: :art: refactor UserStatBlock styles

--- src/components/user-stat-block/index.tsx
+++ src/components/user-stat-block/index.tsx
@@ -9,58 +9,65 @@ interface Props {
   userStats: UserStats
 }
 
-const Content = styled.li`
-  display: grid;
-  grid-gap: 2rem;
-  grid-template-columns: 3.75rem 6rem 8.365rem 3fr;
-  grid-template-areas: 'rank avatar name stats';
-  align-items: center;
-
-  > p {
-    font-size: 2rem;
-    opacity: 0.8;
-    grid-area: rank;
-  }
-  > img {
-    border-radius: 50%;
-    width: 6rem;
-    height: 6rem;
-    grid-area: avatar;
-  }
-  > span {
-    grid-area: name;
-  }
-  > div {
-    grid-area: stats;
-
-    span {
-      font-size: 1.25rem;
-      margin-right: 1rem;
-      opacity: 0.8;
-    }
-  }
-`
-
 export default function UserStatBlock({ rank, userStats }: Props) {
   return (
     <Card>
-      <Content>
-        <p>#{rank}</p>
-        <img src={userStats.avatarUrl} alt={`${userStats.user}'s avatar`} />
-        <span>{userStats.user}</span>
-        <div>
-          <p>
+      <$Content>
+        <$Rank>#{rank}</$Rank>
+        <$User>
+          <img src={userStats.avatarUrl} alt={`${userStats.user}'s avatar`} />
+          <span>{userStats.user}</span>
+        </$User>
+        <$Stats>
+          <div>
             <Emoji as="⚔️" aria-label="crossing-swords" />+{userStats.additions}
             &emsp;-
             {userStats.deletions}
-          </p>
-          <p>
+          </div>
+          <div>
             <Emoji as="💾" aria-label="floppy" />
-            {userStats.commits}
-            {userStats.commits === 1 ? 'Commit' : 'Commits'}
-          </p>
-        </div>
-      </Content>
+            {userStats.commits} {userStats.commits === 1 ? 'Commit' : 'Commits'}
+          </div>
+        </$Stats>
+      </$Content>
     </Card>
   )
 }
+
+const $Content = styled.li`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: var(--space-6) 1fr 1fr;
+  grid-template-areas: 'rank user stats';
+  align-items: center;
+`
+
+const $Rank = styled.span`
+  font-size: var(--h3);
+  opacity: 0.8;
+  grid-area: rank;
+`
+
+const $User = styled.div`
+  grid-area: user;
+  display: flex;
+  align-items: inherit;
+  img {
+    border-radius: 50%;
+    width: 6rem;
+    height: 6rem;
+    margin-right: var(--space-5);
+  }
+`
+
+const $Stats = styled.div`
+  grid-area: stats;
+  display: grid;
+  gap: var(--space-3);
+
+  span {
+    font-size: var(--h5);
+    margin-right: var(--space-3);
+    opacity: 0.8;
+  }
+`
