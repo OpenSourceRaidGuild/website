9e25806274374bb81a75c64ef50d23cebf32d39c
MohitPopli <m.popli5@gmail.com>
2021-02-13 16:29:41 +0800

    Adding left section content including header view and some styles adjustments

--- src/components/homePage/index.tsx
+++ src/components/homePage/index.tsx
@@ -8,7 +8,7 @@ const HomePage = () => {
   return (
     <$HomePageContainer>
       <$LeftContainer>
-        <header>
+        <nav>
           <NavLink to="#">about us</NavLink>
 
           <NavLink to="#">our raids</NavLink>
@@ -16,7 +16,15 @@ const HomePage = () => {
           <NavLink to="#">our stats</NavLink>
 
           <NavLink to="#">contact us</NavLink>
-        </header>
+        </nav>
+        <$LeftContentWrapper>
+          <section>
+            <$MessageLine1>We love to</$MessageLine1>
+            <$MessageLine2>code the</$MessageLine2>
+            <$MessageLine3>future</$MessageLine3>
+          </section>
+          <$JoinUsButton type="button">join us</$JoinUsButton>
+        </$LeftContentWrapper>
       </$LeftContainer>
       <$RightContainer>
         <$BackgroundOverlayContainer>
@@ -38,8 +46,10 @@ const $HomePageContainer = styled.div`
 const $LeftContainer = styled.section`
   flex-grow: 1;
   padding-top: 2rem;
+  display: flex;
+  flex-direction: column;
 
-  header {
+  nav {
     display: flex;
     justify-content: space-evenly;
 
@@ -47,6 +57,9 @@ const $LeftContainer = styled.section`
       text-transform: uppercase;
       text-decoration: none;
       color: #232323;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 16px;
     }
   }
 `
@@ -71,13 +84,86 @@ const $BackgroundOverlayContainer = styled.div`
   top: 36%;
   left: 25%;
 
+  /* Desktops and laptops ----------- */
+  @media only screen and (min-width: 1224px) {
+    top: 36%;
+    left: 21%;
+  }
+
+  /* Large screens ----------- */
+  @media only screen and (min-width: 1824px) {
+    top: 38%;
+    left: 19%;
+  }
+
   div {
     position: absolute;
 
     img {
       width: 22rem;
       height: 22rem;
+      /* Desktops and laptops ----------- */
+      @media only screen and (min-width: 1224px) {
+        width: 25rem;
+        height: 25rem;
+      }
+
+      /* Large screens ----------- */
+      @media only screen and (min-width: 1824px) {
+        width: 25rem;
+        height: 25rem;
+      }
     }
+  }
+`
+
+const $MessageLine1 = styled.span`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 28px;
+  color: #888888;
+`
+
+const $MessageLine2 = styled.span`
+  font-style: normal;
+  font-weight: 900;
+  font-size: 55px;
+  text-transform: uppercase;
+  color: #59c9e6;
+`
+
+const $MessageLine3 = styled.span`
+  font-style: normal;
+  font-weight: 900;
+  font-size: 69px;
+  text-transform: uppercase;
+  color: #101c2e;
+`
+
+const $JoinUsButton = styled.button`
+  border: 2px solid #101c2e;
+  box-sizing: border-box;
+  border-radius: 8px;
+  width: 138px;
+  height: 42px;
+  text-transform: uppercase;
+  cursor: pointer;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 16px;
+`
+
+const $LeftContentWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  padding-left: 5.2em;
+
+  section {
+    display: flex;
+    flex-direction: column;
   }
 `
 export default HomePage
