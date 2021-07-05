c9401994497c1e587d096d8e17ee0e9253631b90
JacobMGEvans <jacobmgevans@gmail.com>
2021-07-05 16:31:10 -0500

    Jest native ESM support for tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,13 @@
 module.exports = {
-  ...require('@snowpack/app-scripts-react/jest.config.js')(),
+  // ...require('@snowpack/app-scripts-react/jest.config.js')(),
   moduleNameMapper: {
     '^#components/(.*)': '<rootDir>/src/components/$1',
     '^#utils/(.*)': '<rootDir>/src/utils/$1',
   },
   setupFilesAfterEnv: ['./src/setupTests.ts'],
+  transform: {},
+
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
 }
 
 /*
