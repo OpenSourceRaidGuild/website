05431f76d03ac496a0b493b1283ef0a93dac017f
JacobMGEvans <jacobmgevans@gmail.com>
2021-07-10 17:00:10 -0500

    prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*.+(js|jsx|ts|tsx)': ['eslint --fix'],
-  '*.+(json|yml|yaml|css|js|jsx|ts|tsx)': ['npx rome check --apply'],
+  '*.+(json|yml|yaml|css|js|jsx|ts|tsx)': ['prettier--write'],
 }
