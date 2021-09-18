fe5aab893ccdf2a06ef8885b4f009b362f923028
theo <me@t3.gg>
2021-09-17 21:05:48 -0700

    Cleanup, copy and credits

--- src/frontend/components/topnav.tsx
+++ src/frontend/components/topnav.tsx
@@ -6,17 +6,23 @@ export default function TopNav() {
   return (
     <nav className="absolute top w-full flex h-24 px-6 uppercase font-bold text-gray-700 z-10 items-center justify-between">
       <div className="flex">
-        <Link href="/about">
-          <a className="link-light">About Us</a>
-        </Link>
+        <a
+          href="https://discord.gg/m5U24z92Va"
+          target="_blank"
+          rel="noreferrer"
+          className="link-light"
+        >
+          Discord
+        </a>
         <div className="pr-4" />
-        <Link href="/raids">
-          <a className="link-light">Our Raids</a>
-        </Link>
-        <div className="pr-4" />
-        <Link href="/stats">
-          <a className="link-light">Our Stats</a>
-        </Link>
+        <a
+          href="https://github.com/OpenSourceRaidGuild"
+          target="_blank"
+          rel="noreferrer"
+          className="link-light"
+        >
+          Github
+        </a>
         <div className="pr-4" />
         <Link href="/contact">
           <a className="link-light">Contact Us</a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,26 @@ export default function Home() {
       <div className="max-w-md md:max-w-2xl text-lg p-4">
         <img src="/logo.svg" alt="logo" className="animate-fade-in-down" />
       </div>
+      <div>
+        <h1 className="text-4xl animate-fade-in-down max-w-lg md:max-w-xl text-center text-gray-700">
+          {"Open Source Raid Guild"}
+        </h1>
+        <div className="pt-4" />
+        <h3 className="text-2xl animate-fade-in-down max-w-lg md:max-w-xl text-center font-light text-gray-700">
+          {"When battling code starts to feel like a raid boss,"}
+          <br />
+          {"we're here to help"}
+        </h3>
+      </div>
+      <div className="absolute bottom-0 w-full text-center font-light">
+        {"Quickly hacked together by "}
+        <a
+          href="https://t3.gg/me"
+          className="link-light text-gray-900 font-normal underline hover:text-blue-800"
+        >
+          Theo
+        </a>
+      </div>
     </div>
   );
 }
