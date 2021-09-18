3afe810cde549d596e9c1d9f1329c427ffe89814
theo <me@t3.gg>
2021-09-17 22:00:17 -0700

    Use env variable for webhook

--- src/backend/utils/ping-discord.ts
+++ src/backend/utils/ping-discord.ts
@@ -1,15 +1,12 @@
 export const sendToDiscord = async (message: string) => {
-  const req = await fetch(
-    "https://discord.com/api/webhooks/888615431617785876/nNcSPs4yZZVqaGc-ohEO0Hw55tqMt7ywntb6R6AOzZfSylw7O2TeGk7lXhMDecKBbPjZ",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        content: message,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const req = await fetch(process.env.DISCORD_WEBHOOK as string, {
+    method: "POST",
+    body: JSON.stringify({
+      content: message,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   return req;
 };
