0fdd3b3af1fa6a66be00d962e82349798bdeefa6
theo <me@t3.gg>
2021-09-17 19:27:48 -0700

    Condense pages

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,20 +1,11 @@
-import { CustomSiteHead } from "../components/head";
-import TopNav from "../components/topnav";
-
-export default function AboutPage() {
+export default function ContactPage() {
   return (
-    <div>
-      <CustomSiteHead />
-      <TopNav />
-      <div
-        id="about"
-        className="flex flex-col items-center min-h-screen justify-center cursor-default relative"
-      >
-        <div className="max-w-md md:max-w-2xl text-lg p-4">
-          <h1 className="animate-fade-in-down text-4xl font-bold">
-            Coming Soon
-          </h1>
-        </div>
+    <div
+      id="about"
+      className="flex flex-col items-center min-h-screen justify-center cursor-default relative"
+    >
+      <div className="max-w-md md:max-w-2xl text-lg p-4">
+        <h1 className="animate-fade-in-down text-4xl font-bold">Coming Soon</h1>
       </div>
     </div>
   );
